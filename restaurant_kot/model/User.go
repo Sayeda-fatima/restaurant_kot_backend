9bff8e3b64f9257129f5440be704028ac38cbcab
Sayeda-fatima <sayedafatima20@gmail.com>
2024-12-02 11:20:03 +0530

    Changes

--- restaurant_kot/model/User.go
+++ restaurant_kot/model/User.go
@@ -11,7 +11,7 @@ type User struct {
 	RestaurantID   uint         `json:"restaurant_id" gorm:"not null"`
 	Restaurant     Restaurant   `json:"-" gorm:"foreignKey:RestaurantID;references:ID" validate:"-"`
 	Name           string       `json:"name" gorm:"not null;size:255"`
-	Email          string       `json:"email" gorm:"unique" validate:"required,email"`
+	Email          string       `json:"email" gorm:"not null;unique" validate:"required,email"`
 	Password       string       `json:"password" gorm:"not null;size:255" validate:"required,min=8"`
 	AccessType     string       `json:"access_type" gorm:"not null;type:enum('admin','manager','staff')"`
 	ApiToken       string       `json:"api_token" gorm:"size:255"`
